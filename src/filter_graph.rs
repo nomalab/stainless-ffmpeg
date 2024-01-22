2150e467d3c0657262602fdce40217a7435f689b
Bramart <martial@nomalab.com>
2024-01-22 16:17:41 +0100

    get frame from buffersink direct after frame was added

--- src/filter_graph.rs
+++ src/filter_graph.rs
@@ -253,7 +253,25 @@ impl FilterGraph {
             }
           }
         }
+        for (index, output_filter) in self.audio_outputs.iter().enumerate() {
+          let mut result = 0;
+          while result >= 0 {
+            let output_frame = av_frame_alloc();
+            result = av_buffersink_get_frame(output_filter.context, output_frame);
+            if result == AVERROR(EAGAIN) || result == AVERROR_EOF {
+              break;
+            } else {
+              check_result!(result);
+            }
+            output_audio_frames.push(Frame {
+              name: Some(output_filter.get_label()),
+              frame: output_frame,
+              index,
+            });
+          }
+        }
       }
+
       for frame in in_video_frames {
         for input in &self.video_inputs {
           if let Some(label) = &frame.name {
@@ -265,41 +283,22 @@ impl FilterGraph {
             }
           }
         }
-      }
-
-      for (index, output_filter) in self.audio_outputs.iter().enumerate() {
-        let mut result = 0;
-        while result >= 0 {
-          let output_frame = av_frame_alloc();
-          result = av_buffersink_get_frame(output_filter.context, output_frame);
-          if result == AVERROR(EAGAIN) || result == AVERROR_EOF {
-            break;
-          } else {
-            check_result!(result);
+        for (index, output_filter) in self.video_outputs.iter().enumerate() {
+          let mut result = 0;
+          while result >= 0 {
+            let output_frame = av_frame_alloc();
+            result = av_buffersink_get_frame(output_filter.context, output_frame);
+            if result == AVERROR(EAGAIN) || result == AVERROR_EOF {
+              break;
+            } else {
+              check_result!(result);
+            }
+            output_video_frames.push(Frame {
+              name: Some(output_filter.get_label()),
+              frame: output_frame,
+              index,
+            });
           }
-          output_audio_frames.push(Frame {
-            name: Some(output_filter.get_label()),
-            frame: output_frame,
-            index,
-          });
-        }
-      }
-
-      for (index, output_filter) in self.video_outputs.iter().enumerate() {
-        let mut result = 0;
-        while result >= 0 {
-          let output_frame = av_frame_alloc();
-          result = av_buffersink_get_frame(output_filter.context, output_frame);
-          if result == AVERROR(EAGAIN) || result == AVERROR_EOF {
-            break;
-          } else {
-            check_result!(result);
-          }
-          output_video_frames.push(Frame {
-            name: Some(output_filter.get_label()),
-            frame: output_frame,
-            index,
-          });
         }
       }
     }
